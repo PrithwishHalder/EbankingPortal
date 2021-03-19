65c73f31fd27e2ce8ac2ca40c59c0444eedf482d
PrithwishHalder <halderprithwish@gmail.com>
2021-03-20 00:29:11 +0530

    clerk fixes and two pages

--- clerk/urls.py
+++ clerk/urls.py
@@ -3,6 +3,17 @@ from clerk import views
 urlpatterns = [
 
     path('clrk/<id>', views.clrk_dashboard, name="clrk_dashboard"),
+    path('clrk_addUser/<id>', views.clrk_adduser, name="addUser"),
+    path('clrk_searchUser/<id>', views.clrk_searchUser, name="searchUser"),
+    path('clrk_updateUser/<id>', views.clrk_updateUser, name="updateUser"),
+    path('clrk_userTransaction/<id>',
+         views.clrk_userTransaction, name="userTransaction"),
+    path('ajaxTransaction/<accno>',
+         views.clrk_ajaxTransaction, name="ajaxTransaction"),
+    path('clrk_Withdrawl/<id>', views.clrk_Withdrawl, name="clrkWithdrawl"),
+    path('clrk_Deposit/<id>', views.clrk_Deposit, name="clrkDeposit"),
+    path('clrk_Transfer/<id>', views.clrk_Transfer, name="clrkTransfer"),
+    path('clrkprofile/<id>', views.clrkprofile, name="cprofile"),
     path('clrklogout/', views.clogout, name="clogout"),
 
 ]
--- clerk/views.py
+++ clerk/views.py
@@ -1,10 +1,122 @@
 from django.shortcuts import render, redirect
+from user.models import User, Transaction
+from .models import Clerk
 
 # Create your views here.
 
 
 def clrk_dashboard(request, id):
-    return render(request, "cdashboard.html", {})
+
+    if request.method == 'GET':
+        if request.session.has_key('clerkid'):
+            return render(request, "cdashboard.html", {})
+        else:
+            return redirect('clerk')
+
+
+def clrk_adduser(request, id):
+    if request.method == 'GET':
+        if request.session.has_key('clerkid'):
+            return render(request, "adduser.html", {})
+        else:
+            return redirect('clerk')
+
+
+def clrk_searchUser(request, id):
+    if request.method == 'GET':
+        if request.session.has_key('clerkid'):
+            return render(request, "searchuser.html", {})
+        else:
+            return redirect('clerk')
+
+
+def clrk_updateUser(request, id):
+    if request.method == 'GET':
+        if request.session.has_key('clerkid'):
+            return render(request, "updateuser.html", {})
+        else:
+            return redirect('clerk')
+
+
+def clrk_userTransaction(request, id):
+    if request.method == 'GET':
+        if request.session.has_key('clerkid'):
+            return render(request, "usertransaction.html", {})
+        else:
+            return redirect('clerk')
+
+
+def clrk_ajaxTransaction(request, accno):
+
+    context = {}
+
+    tran = list(Transaction.objects.filter(accountno=accno))
+    if tran:
+        context['trans'] = tran
+    else:
+        context['transerror'] = "Account No Invalid!"
+
+    if request.method == 'GET':
+        if request.session.has_key('clerkid'):
+            return render(request, "ajaxtransaction.html", context)
+        else:
+            return redirect('clerk')
+
+
+def clrk_Withdrawl(request, id):
+    if request.method == 'GET':
+        if request.session.has_key('clerkid'):
+            return render(request, "cashwithdrawl.html", {})
+        else:
+            return redirect('clerk')
+
+
+def clrk_Deposit(request, id):
+    if request.method == 'GET':
+        if request.session.has_key('clerkid'):
+            return render(request, "deposit.html", {})
+        else:
+            return redirect('clerk')
+
+
+def clrk_Transfer(request, id):
+    if request.method == 'GET':
+        if request.session.has_key('clerkid'):
+            return render(request, "moneytransfer.html", {})
+        else:
+            return redirect('clerk')
+
+
+def clrkprofile(request, id):
+
+    context = {}
+
+    clerk = Clerk.objects.get(id=id)
+    context['data'] = clerk
+
+    if request.method == 'POST':
+        opass = request.POST['opassword']
+        npass = request.POST['npassword']
+        cpass = request.POST['cpassword']
+        if npass == cpass:
+            try:
+                clerk = Clerk.objects.get(id=id, password=opass)
+            except:
+                context['profilemsg'] = "Old Password did not match!"
+            else:
+                clerk.password = npass
+                clerk.save()
+                context['profilemsg'] = "Password Changed!"
+        else:
+            context['profilemsg'] = "New Password did not match!"
+
+        return render(request, "clerkprofile.html", context)
+
+    if request.method == 'GET':
+        if request.session.has_key('clerkid'):
+            return render(request, "clerkprofile.html", context)
+        else:
+            return redirect('clerk')
 
 
 def clogout(request):
